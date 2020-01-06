386a18e39149f85b12c6853ddbf235ec1d9a5340
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-01-06 00:57:08 -0500

    Updates

---
+++ precache-manifest.c47d121a8b6a2e6e86d8e30957056c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d09d968a0034ef39189aeb52756ba2b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4ba496535e2ee90a33a4",
+    "url": "/monster-rolodex/static/css/main.7d398e37.chunk.css"
+  },
+  {
+    "revision": "b62b9052603d1f962399",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ba496535e2ee90a33a4",
+    "url": "/monster-rolodex/static/js/main.9ce15a0e.chunk.js"
+  },
+  {
+    "revision": "99047a773d79ca6f96f0",
+    "url": "/monster-rolodex/static/js/runtime-main.ec5f649a.js"
+  }
+]);
